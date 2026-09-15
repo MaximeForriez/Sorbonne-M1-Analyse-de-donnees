203b452edf401e7f1283d2ca499e8a524946f219
Maxime Forriez <162214241+MaximeForriez@users.noreply.github.com>
2026-09-15 15:04:34 +0200

    docs:correction of all repository for the courses of 2026-2027

---
+++ Seance-07/Exercice-EnviTERR/src/main.py
@@ -0,0 +1,53 @@
+#coding:utf8
+
+import numpy as np
+import pandas as pd
+import scipy
+import scipy.stats
+
+def ouvrirUnFichier(nom):
+    with open(nom, "r", encoding = "utf-8") as fichier:
+        contenu = pd.read_csv(fichier)
+    return contenu
+
+def tableauDeContingence(nom, donnees):
+    indexValeurs = {}
+    for element in range(0,len(nom)):
+        indexValeurs.update({element: nom[element]})
+    return pd.DataFrame(donnees).rename(index = indexValeurs)
+
+def sommeDesColonnes(tableau):
+    colonne = list(tableau.head(0))
+    sommeColonne = []
+    for element in colonne:
+        sommeColonne.append(tableau[element].sum())
+    return sommeColonne
+
+def sommeDesLignes(tableau):
+    colonne = list(tableau.head(0))
+    sommeLigne = []
+    for element1 in range(0,len(tableau)):
+        ligne = []
+        for element2 in range(0,len(colonne)):
+            ligne.append(tableau.iloc[element1, element2])
+        sommeLigne.append(np.sum(list(ligne)))
+    return sommeLigne
+
+# Création du tableau de contingence
+# Contrairement à l'usage, vous ne devez pas créer de tableau croisé dynamique, puisque le fichier est déjà un tableau de contingence
+
+# Question 1
+print("Question 1")
+# Calculer les marges
+
+# Question 2
+print("Question 2")
+
+# Question 3
+print("Question 3")
+# Faire le test du chi2 avec les outils Scipy.stats
+
+# Question 4
+
+# Question bonus
+print("Question bonus")
