0585f0e7ac158514d5eceb65ecddda8db3184737
Maxime Forriez <162214241+MaximeForriez@users.noreply.github.com>
2026-09-22 13:53:54 +0200

    docs:correction of all repository for the courses of 2026-2027

---
+++ Seance-05/Exercice-GeoSUDS/src/main.py
@@ -0,0 +1,94 @@
+#coding:utf8
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy
+import scipy.stats
+import math
+
+def ouvrirUnFichier(nom):
+    with open(nom, "r", encoding='utf-8') as fichier:
+        contenu = pd.read_csv(fichier)
+    return contenu
+
+def conversionLog(liste):
+    log = []
+    for element in liste:
+        log.append(math.log(element))
+    return log
+
+def ordreDecroissant(liste):
+    liste.sort(reverse = True)
+    return liste
+
+def ordrePopulation(pop, etat):
+    ordrepop = []
+    for element in range(0, len(pop)):
+        if np.isnan(pop[element]) == False:
+            ordrepop.append([float(pop[element]), etat[element]])
+    ordrepop = ordreDecroissant(ordrepop)
+    for element in range(0, len(ordrepop)):
+        ordrepop[element] = [element + 1, ordrepop[element][1]]
+    return ordrepop
+
+def classementPays(ordre1, ordre2):
+    classement = []
+    if len(ordre1) <= len(ordre2):
+        for element1 in range(0, len(ordre2) - 1):
+            for element2 in range(0, len(ordre1) - 1):
+                if ordre2[element1][1] == ordre1[element2][1]:
+                    classement.append([ordre1[element2][0], ordre2[element1][0], ordre1[element2][1]])
+    else:
+        for element1 in range(0, len(ordre1) - 1):
+            for element2 in range(0, len(ordre2) - 1):
+                if ordre2[element2][1] == ordre1[element1][1]:
+                    classement.append([ordre1[element1][0], ordre2[element2][0], ordre1[element][1]])
+    return classement
+
+# Question 2
+print("Question 2")
+
+# "year" : année
+# "origin_id" : code ISO du territoire d'origine
+# "export_val" : exportations en USD
+# "import_val" : importations en USD
+# "export_ton" : exportations en tonnes
+# "import_ton" : importations en tonnes
+
+# Question 3
+print("Question 3")
+
+# Question 4
+print("Question 4")
+
+# Question 5
+print("Question 5")
+
+# Question 6
+print("Question 6")
+
+# Question 7
+print("Question 7")
+# Mettre ici votre réponse argumentée
+
+# Question 9
+print("Question 9")
+
+# Question 10
+print("Question 10")
+
+# Question 11
+print("Question 11")
+
+# Question 12
+print("Question 12")
+
+# Question 13
+print("Question 13")
+
+# Question 14
+print("Question 14")
+
+# Question Bonus
+print("Question Bonus")
